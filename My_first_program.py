2f105b82b0d254b061c8314e356a3450f8463f8d
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-25 19:16:21 +0530

    My_first_program

---
+++ My_first_program.py
@@ -0,0 +1,28 @@
+# MY FIRST PYTHON PROGRAM (ANPNA COLLEGE 25/03/2026)
+print("Hello World")
+print('Sudarshan.R.S')
+print (f'Sapthagiri NPS University')
+M = 'Data Analyst'
+print (f'Sudarshana.R.S is a {M}')
+print("My name is Sudarshana.R.S." , 'I was born on 25th September 2007.', 'I am 18 years old.')
+print (25092007)
+print (25+9+2007)
+print('25+09+2007')
+name = 'Sudarshana.R.S'
+print(name)
+age = 18
+college = 'Bangalore'
+print(name,age,college)
+print('sudarshan07')
+num= '34'
+print(type(num))
+print(type(age))
+# nam=input(str('Enter your name: \t'))
+# print(f'Your name is {nam}')
+# input('Enter the name: ')
+# print(type(input))
+print ('my name is ', name)
+print(name==age )
+age1=age
+print(''' My age is ''', age1) 
+print('Hello India')
