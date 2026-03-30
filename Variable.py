03d22f4258679909abe7970df88678224cbfca69
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-30 19:09:37 +0530

    3rd commit

---
+++ Variable.py
@@ -0,0 +1,10 @@
+# Lecture on Variable 30/03/2025
+name="Sudarshana.R.S"
+age=18
+college='Sapthagiri NPS College'
+place=''' Bangalore '''
+print('Name: ', name)
+print('Age: ',age)
+print('College: ',college)
+print('Place: ', place)
+print(f"My name is {name}.I am {age} years old, I pursing my Bachulor's at {college}, {place}")
