7156b8759fe4ed386bafe0c6d1e42b2afb4354ac
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-30 19:18:32 +0530

    7th commit

---
+++ Type_function.py
@@ -0,0 +1,11 @@
+# Type function in Python 30/03/2026
+# Type is a function used in python to know the data type of the particular variables
+
+name='Sudarshana'
+age=18
+p='B'
+price=25.9
+print(type(name))
+print(type(age))
+print(type(p))
+print(type(price))
