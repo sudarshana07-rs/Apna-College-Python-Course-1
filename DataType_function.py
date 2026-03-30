d52584fe966d1d9cf5f8626d51ac747ae700ec44
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-30 19:28:52 +0530

    9th commit

--- DataType_function.py
+++ DataType_function.py
@@ -1,6 +1,7 @@
 # Type function in Python 30/03/2026
 # Type is a function used in python to know the data type of the particular variables
-
+# Datatypes in PYTHON
+# Interge, String, Float, Boolean, NONE
 name='Sudarshana'
 age=18
 p='B'
@@ -9,3 +10,7 @@ print(type(name))
 print(type(age))
 print(type(p))
 print(type(price))
+boo=False
+boo1=True
+print(type(boo))
+print(type(boo1))
