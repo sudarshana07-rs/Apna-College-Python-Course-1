9cf398b604cc812c681cc62eab2df2da78f12e2f
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-03-30 19:43:30 +0530

    11th commit

---
+++ Exercise1.py
@@ -0,0 +1,6 @@
+# Question: Print sum of two numbers
+
+num1=int(input("Enter the first number: "))
+num2=int(input("Enter the second number: "))
+sum= num1+num2
+print(f'Sum of {num1} and {num2} is {sum}')
