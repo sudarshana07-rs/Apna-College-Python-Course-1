58165fef8d04150d593e51f49ee91d322abedf72
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-01 18:11:02 +0530

    13th commit

---
+++ Comments.py
@@ -0,0 +1,6 @@
+print("Comment Line")
+# Comments are the lines which are not executed by the compiler
+""" Comments will improve the readability 
+and helps us to document our work. It can be
+used to write the logic or any important explaination 
+about the program or project. """
