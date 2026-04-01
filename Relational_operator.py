a46ff002ddc59005dcdbfc3fd0095151d289ddf8
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-01 18:29:15 +0530

    17th commit

---
+++ Relational_operator.py
@@ -0,0 +1,9 @@
+#Relational / Comparission Operators
+a=int(input("Enter the first number: "))
+b=int(input("Enter the second number: "))
+print(a==b) 
+print(a!=b)
+print(a>b)
+print(a>=b)
+print(a<b)
+print(a<=b)
