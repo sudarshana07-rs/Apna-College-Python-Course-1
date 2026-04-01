c24aa52113b689a4a1d49eef9a80fde7a3859ae2
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-01 19:18:49 +0530

    19th commit

---
+++ Logical_operators.py
@@ -0,0 +1,21 @@
+# Logical Operators 01/04/2026
+# Logical "and"
+username=input("Enter the username: ")
+password=int(input("Enter the password: "))
+username1='Sudarshana07'
+password1=25092007
+print(username==username1 and password==password1)
+
+#Logical "or"
+username=input("Enter the username: ")
+password=int(input("Enter the password: "))
+username1='suresh71'
+password1=3091971
+print(username==username1 or username==username1)
+
+#Logical "not"
+username="suresh71"
+password=1234
+username1="suresh71"
+password1=1234
+print(not (username == username1))
