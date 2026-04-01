99a18358eac34bec714c4298c126c7aa543704a5
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-01 18:21:17 +0530

    15th commit

---
+++ Arithmetic_operators.py
@@ -0,0 +1,9 @@
+n1=int(input("Enter the first number: "))
+n2=int(input("Enter the second number: "))
+print(n1-n2)
+print(n1+n2)
+print(n1*n2)
+print(n1/n2)
+print(n1//n2) #It gives the number before the point
+print(n1%n2) #It gives the remainder
+print(n1**2) #It gives the square
