1ca254ddf032194f4302741ddec9c12245ee2c66
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-01 19:36:45 +0530

    21th commit

---
+++ Type_casting.py
@@ -0,0 +1,17 @@
+# Type Conversion and Type Casting 01/04/2026
+
+# Type Conversion
+# Type Conversion is nothing but the compiler converts the data type automatically
+# Here 'a' is in int and 'b' is in float the compiler coverts a into float
+# int is converted to float becoz float is of higher order than int
+a=3
+b=2.5
+print(a+b)
+
+# Type Casting
+# Type Casting is that the programmer converts one datatype to another datatype
+a=3
+b=2.5
+a1=float(a)
+print(a1)
+print(a+b)
