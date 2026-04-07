310662473a9f0931003847230071818f9027e6b2
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 18:40:23 +0530

    32nd commit

--- String_opetations.py
+++ String_opetations.py
@@ -7,3 +7,16 @@ str1="Apna"
 str2="College"
 final_str=str1+str2
 print(f'The concatenated string is {final_str}.')
+final=str1+" " +str2
+print(final)
+
+# 2. Length of string
+s1="Sudarshana"
+s2="Reddy"
+f=s1+" "+s2
+length1=len(s1)
+length2=len(s2)
+length3=len(f)
+print(length1)
+print(length2)
+print(length3)
