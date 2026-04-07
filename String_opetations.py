e701539726b7eb7aae21732b90e33a0e28cfd90b
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 18:34:05 +0530

    31th commit

--- String_opetations.py
+++ String_opetations.py
@@ -1,1 +1,9 @@
 # String Operations 07/04/2026
+# 1. Concatenation
+# 2. Length of string
+
+# 1.Concatenation: Adding two strings.
+str1="Apna"
+str2="College"
+final_str=str1+str2
+print(f'The concatenated string is {final_str}.')
