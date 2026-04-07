c49d838beb1357d41777246b72b8e088f112ff55
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 18:25:29 +0530

    28th commit

--- Strings.py
+++ Strings.py
@@ -3,3 +3,10 @@ str2="I live in Bangalore."
 str3='''I study in Sapthagiri NPS University.'''
 str4="This is Sudarshana's book." 
 str5= '''Sudarshana said "Work Hard In Silence Let Success make noise."'''
+
+# Escape sequences: These are the sequences which gives formatting
+# For example, new line , tab space, etc
+
+# Escape Sequence: Next Line \n
+str="My name is Sudarshana.R.S. \nI am 18 years old."
+print(str)
