fd04eac102765569a0ae1870594660e8277d2beb
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 19:06:45 +0530

    35th commit

--- String_indexing.py
+++ String_indexing.py
@@ -1,6 +1,11 @@
 # String Indexing 07/04/2026
 # String indexing is the number alloted to the characters in string
+# string indexing "Helps to access the characters in a string."
 # S U D A R S H A N A
 # 0 1 2 3 4 5 6 7 8 9
 # str[0]='S' , str[6]='H'
 
+str='Sudarshana.R.S'
+index= str[10]
+print(index)
+print(str[4])
