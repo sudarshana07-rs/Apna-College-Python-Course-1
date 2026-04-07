eb1ba46e7932eb5b952dfa91ee3861928044df57
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 19:25:42 +0530

    37th commit

---
+++ String_Slicing.py
@@ -0,0 +1,13 @@
+#  String Slicing 07/04/2026
+# String Slicing: Accessing parts of string
+# syntax: str[starting_index:ending_index]
+
+str="Sudarsahana.R.S"
+final=str[2:5]
+print(final) 
+
+str2='Apna College'
+print(len(str2))
+print(str2[5:len(str)])
+print(str2[5:12])
+print(str2[0:4])
