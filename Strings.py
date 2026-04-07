e5c225a184a962913d62d6d52ae122af4fc54b52
sudarshana07-rs <sudarshana.rs07@gmail.com>
2026-04-07 18:19:58 +0530

    27th commit

--- Strings.py
+++ Strings.py
@@ -1,3 +1,5 @@
 str1='My Name is Sudarshana.R.S'
 str2="I live in Bangalore."
 str3='''I study in Sapthagiri NPS University.'''
+str4="This is Sudarshana's book." 
+str5= '''Sudarshana said "Work Hard In Silence Let Success make noise."'''
